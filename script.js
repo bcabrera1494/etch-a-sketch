7032b8f43e4b6ea5f2654232aaf23827710a845d
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-23 16:24:26 -0700

    Declare height and width of div to contain grid in 16x16px.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const body = document.querySelector('body');
 document.addEventListener("DOMContentLoaded", function() {
     // Get the container element
     const gridContainer = document.querySelector('#container');
-    gridContainer.setAttribute()
+   // gridContainer.setAttribute();
     body.appendChild(gridContainer);
 
     // Create a 16x16 grid
@@ -13,12 +13,10 @@ document.addEventListener("DOMContentLoaded", function() {
     // Loop to create grid items and append them to the body
     for (let i = 0; i < rows; i++) {
         for (var j = 0; j < columns; j++) {
-            var gridItem = document.createElement("div");
+            const gridItem = document.createElement("div");
             gridItem.classList.add("grid-item");
 
             // Set the position of each grid item
-            gridItem.style.left = j * 100 + "px";
-            gridItem.style.top = i * 100 + "px";
 
             // Append the grid item to the container div
             gridContainer.appendChild(gridItem);
