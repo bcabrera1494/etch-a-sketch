c54f910f5a71f4c96e0f6544a789651e0047a3a3
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-24 15:48:13 -0700

    Write function to remove grid. Call removeGrid function under button event listener on click. Call newGrid function to create new grid based on user input. Removes the old grid as expected. Creates a new grid, but formatting is incorrect. Need to specify pixels of div holding grid cells to be fixed and have the grid cells fit to the defined pixel space of the container div.

--- script.js
+++ script.js
@@ -3,29 +3,40 @@ const body = document.querySelector('body');
 // create button that will send the user a popup asking for the number of squares per side for the new grid.
 const button = document.createElement('button');
 body.appendChild(button);
-button.textContent = 'Click here to set the size of your grid!';
+button.textContent = 'Customize your grid!';
+
+// function to make grid
+function newGrid(x){
+    // logic to remove original grid or prevent original grid from loading could go here
+    const gridContainer = document.querySelector('#container');
+    for (let r = 0; r < x; r++) {
+        for (let c = 0; c < x; c++) {
+            const gridItem = document.createElement("div");
+            gridItem.classList.add("grid-item");
+            gridContainer.appendChild(gridItem);
+        };
+    };
+};
+
+// function to remove old grid
+function removeGrid (element){
+    const gridContainer = document.querySelector('#container');
+    gridContainer.removeChild(element)
+};
 
 // add event listener to button that will prompt the user to input a number, which will define the grid dimensions.
 button.addEventListener('click', () => {
+    // remove each cell using .forEach
+    const cells = document.querySelectorAll('.grid-item');
+        cells.forEach(cell => {
+        removeGrid(cell);
+        });
     let userInput = prompt('Enter the number of rows and columns for your grid');
     userInput;
-
-    makeGrid(userInput);
+    newGrid(userInput);
 });
-// function to make grid
-    function makeGrid(x){
-        const gridContainer = document.querySelector('#container');
-        rows = x;
-        columns = x;
-        for (let r = 0; r < x; r++) {
-            for (let c = 0; c < x; c++) {
-                const gridItem = document.createElement("div");
-                gridItem.classList.add("grid-item");
-                gridContainer.appendChild(gridItem);
-            };
-        };
-    };
-// create the grid
+
+// create the original grid
 document.addEventListener("DOMContentLoaded", function () {
     // Get the container element
     const gridContainer = document.querySelector('#container');
@@ -50,6 +61,7 @@ document.addEventListener("DOMContentLoaded", function () {
         };
     };
 });
+
 
 // below works to shade individual elements gray
     const container = document.querySelector('#container');
